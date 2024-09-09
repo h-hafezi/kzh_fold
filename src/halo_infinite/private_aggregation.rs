0e7a36372f2e284c5c3bfd28bacf59122821c7bc
George Kadianakis <desnacked@riseup.net>
2024-09-09 18:50:11 +0300

    halo_infinite: Implement private aggregation

--- src/halo_infinite/private_aggregation.rs
+++ src/halo_infinite/private_aggregation.rs
@@ -1,14 +1,17 @@
 #![allow(unused)]
 use itertools::izip;
 use ark_poly::{DenseUVPolynomial, Polynomial};
+use ark_ec::AffineRepr;
 use ark_ec::VariableBaseMSM;
 use ark_ec::pairing::Pairing;
 use ark_ff::{Field, One, batch_inversion};
 use ark_ff::Zero;
 use ark_ec::CurveGroup;
 use ark_poly::{univariate::DensePolynomial, GeneralEvaluationDomain, EvaluationDomain};
+use crate::kzg::KZGProof;
+use crate::kzg::{KZGCommitment, KZGPowers, KZGVerifierKey, KZG10};
 
-use std::ops::{Div, Mul, Add};
+use std::ops::{Div, Mul, Add, Sub, Neg};
 
 use transcript::IOPTranscript;
 
@@ -36,6 +39,12 @@ where
         .collect()
 }
 
+pub struct PrivateAggregationProof<E: Pairing> {
+    q_commitment: KZGCommitment<E>,
+    proof_g_at_r: KZGProof<E>,
+    g_commitment: KZGCommitment<E>, // XXX remove
+}
+
 /// Prove that f_i(w_i) = 0
 /// Simplification over paper: omega_i is a single element
 pub fn prove<E: Pairing>(
@@ -43,7 +52,8 @@ pub fn prove<E: Pairing>(
     vec_omega: &Vec<E::ScalarField>,
     entire_domain: &GeneralEvaluationDomain<E::ScalarField>,
     transcript: &mut IOPTranscript<E::ScalarField>,
-) -> () {
+    ck: &KZGPowers<'_, E>,
+) -> PrivateAggregationProof<E> {
     let z_poly = vanishing_polynomial::<E>(&vec_omega);
 
     // This is a vector of Omega_i
@@ -74,34 +84,151 @@ pub fn prove<E: Pairing>(
 
     // Step 3: Compute q(x)
     let mut q_x = DensePolynomial::from_coefficients_vec(vec![E::ScalarField::zero()]);
-    for (rho_i, f_i, z_i) in izip!(vec_rho, vec_f, vec_z_i) {
-        let mut numerator = DensePolynomial::from_coefficients_vec(vec![E::ScalarField::zero()]);
-        numerator = numerator.mul(f_i);
+    for (rho_i, f_i, z_i) in izip!(vec_rho.clone(), vec_f.clone(), vec_z_i.clone()) { // XXX loose the clone
+        // Compute rho^{i-1}*f_i
+        let mut numerator = DensePolynomial::from_coefficients_vec(
+            f_i.coeffs.iter().map(|f| *f * rho_i).collect(),
+        );
+        // Compute the entire numerator
         numerator = numerator.mul(&z_i);
 
         q_x = q_x.add(numerator);
     }
     q_x = q_x.div(&z_poly);
+
+    // Step 4: Commit to q(x)
+    let (q_commitment, q_blinder) = KZG10::<E,DensePolynomial<<E as Pairing>::ScalarField>>::commit(&ck, &q_x, None, None).expect("q commitment failed");
+
+    // Step 5: Get r from verifier
+    transcript.append_serializable_element(b"q_comm", &q_commitment.0).unwrap();
+    let r: E::ScalarField = transcript.get_and_append_challenge(b"r").unwrap();
+
+    // Step 6: Compute g(x) [NOT NEEDED]
+    let mut g_x  = DensePolynomial::from_coefficients_vec(vec![E::ScalarField::zero()]);
+    for (rho_i, f_i, z_i) in izip!(vec_rho, vec_f, vec_z_i) {
+        let z_i_r = z_i.evaluate(&r);
+        let f_i_times_z_i_r = DensePolynomial::from_coefficients_vec(
+            f_i.coeffs.iter().map(|f| *f * z_i_r).collect(),
+        );
+        let summand = DensePolynomial::from_coefficients_vec(
+            f_i_times_z_i_r.coeffs.iter().map(|f| *f * rho_i).collect(),
+        );
+        g_x = g_x.add(summand);
+    }
+    let z_r = z_poly.evaluate(&r);
+    let q_x_times_z_r = DensePolynomial::from_coefficients_vec(
+        q_x.coeffs.iter().map(|q| *q * z_r).collect(),
+    );
+    g_x = g_x.sub(&q_x_times_z_r);
+
+    assert_eq!(g_x.evaluate(&r), E::ScalarField::zero());
+
+    let (g_commitment, g_blinder) = KZG10::<E,DensePolynomial<<E as Pairing>::ScalarField>>::commit(&ck, &g_x, None, None).expect("g commitment failed");
+    let proof_g_at_r = KZG10::<E,DensePolynomial<<E as Pairing>::ScalarField>>::open(&ck, &g_x, r, &g_blinder).expect("Proof generation failed");
+
+    PrivateAggregationProof {
+        q_commitment,
+        proof_g_at_r,
+        g_commitment,
+    }
 }
 
+/// Prove that f_i(w_i) = 0
+/// Simplification over paper: omega_i is a single element
+pub fn verify<E: Pairing>(
+    proof: PrivateAggregationProof<E>,
+    vec_f_commitments: &Vec<KZGCommitment<E>>,
+    vec_omega: &Vec<E::ScalarField>,
+    entire_domain: &GeneralEvaluationDomain<E::ScalarField>,
+    transcript: &mut IOPTranscript<E::ScalarField>,
+    vk: &KZGVerifierKey<E>
+) -> bool {
+    let n = vec_f_commitments.len();
+
+    // Step 1: Compute all the domains and polys
+    let z_poly = vanishing_polynomial::<E>(&vec_omega);
+
+    // This is a vector of Omega_i
+    let mut vec_omega_complements = vec![];
+    for omega in vec_omega {
+        let vec_omega_complement_i = remove_from_set(entire_domain.elements().into_iter(), omega);
+        vec_omega_complements.push(vec_omega_complement_i);
+    }
+
+    // Compute z_i(x) = \prod (x - w_i)
+    let mut vec_z_i = vec![];
+    for vec_omega_complement in &vec_omega_complements {
+        let z_i_poly = vanishing_polynomial::<E>(&vec_omega_complement);
+        vec_z_i.push(z_i_poly);
+    }
+
+    // Step 2: Get a challenge from the verifier
+    transcript.append_serializable_element(b"z_x", &z_poly).unwrap();
+    transcript.append_serializable_element(b"z_more", &vec_omega_complements).unwrap();
+    transcript.append_serializable_element(b"z_moremore", &vec_z_i).unwrap();
+    let rho: E::ScalarField = transcript.get_and_append_challenge(b"rho").unwrap();
+
+    // Compute powers of rho
+    let vec_rho = compute_powers(&rho, n);
+
+    assert_eq!(vec_rho.len(), n);
+    assert_eq!(n, vec_z_i.len());
+
+    // Step 2: Get r from verifier
+    transcript.append_serializable_element(b"q_comm", &proof.q_commitment.0).unwrap();
+    let r: E::ScalarField = transcript.get_and_append_challenge(b"r").unwrap();
+
+    let mut C_prime = E::G1Affine::zero();
+    for (rho_i, z_i, C_i) in izip!(vec_rho.clone(), vec_z_i.clone(), vec_f_commitments.clone()) { // XXX loose the clone
+        let z_i_at_r = z_i.evaluate(&r);
+        C_prime = (C_prime + C_i.0.mul(z_i_at_r * rho_i)).into();
+    }
+
+    let z_at_r = z_poly.evaluate(&r);
+    let z_r_C_q = proof.q_commitment.0.mul(z_at_r);
+    let C_g: E::G1Affine = C_prime.sub(z_r_C_q).into_affine();
+
+    // TODO: bug! why need to negate? forgot something.
+    let tmp_negated: E::G1Affine = E::G1Affine::zero().sub(C_g).into();
+    let C_g_commitment: KZGCommitment<E> = KZGCommitment(tmp_negated.into());
+    assert_eq!(C_g_commitment.0, proof.g_commitment.0);
+
+    let zero = E::ScalarField::zero();
+    let is_valid = KZG10::<E, DensePolynomial<<E as Pairing>::ScalarField>>::check(&vk, &C_g_commitment, r, zero, &proof.proof_g_at_r).expect("Verification failed");
+    assert!(is_valid);
+
+    true
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::kzg::{trim};
+
     use super::*;
-    use ark_bn254::{Bn254, Fr, G1Affine, G1Projective};
+    use ark_bn254::{Bn254, Fr};
     use ark_poly::DenseUVPolynomial;
     use std::ops::Sub;
-    use rand::thread_rng;
+    use rand::{rngs::StdRng, thread_rng, SeedableRng};
+
+    type E = Bn254;
 
     #[test]
     pub fn test_prove() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+
         let N = 2;
         let d = 16;
 
-        let mut transcript_prover = IOPTranscript::<Fr>::new(b"ipa");
+        let mut transcript_prover = IOPTranscript::<Fr>::new(b"pa");
+        let mut transcript_verifier = IOPTranscript::<Fr>::new(b"pa");
 
         let mut vec_f : Vec<DensePolynomial<Fr>> = vec![];
+        let mut vec_f_commitments : Vec<KZGCommitment<E>> = vec![];
         let mut vec_omega = vec![];
+
+        // XXX too many powers of tau but it's ok
+        let params = KZG10::<Bn254, DensePolynomial<<E as Pairing>::ScalarField>>::setup(2*d, false, &mut rng).expect("Setup failed");
+        let (ck, vk) = trim(&params, 2*d);
 
         // we want to prove that N polynomials evaluate to t_i at w_i
         // so p_i(w_i) = t_i
@@ -114,15 +241,20 @@ mod tests {
         }
 
         for i in 0..N {
-            let p_poly = DensePolynomial::rand(d, &mut thread_rng());
+            let p_poly = DensePolynomial::rand(d, &mut rng);
             let p_poly_at_w_i = p_poly.evaluate(&vec_omega[i]);
             let f_poly = p_poly.sub(&DensePolynomial::from_coefficients_slice(&[p_poly_at_w_i]));
             // Check that f_i(w_i) == 0
             assert_eq!(Fr::zero(), f_poly.evaluate(&vec_omega[i]));
-            vec_f.push(f_poly);
+            vec_f.push(f_poly.clone());
+
+            let (f_commitment, _) = KZG10::<E,DensePolynomial<<E as Pairing>::ScalarField>>::commit(&ck, &f_poly, None, None).expect("f commitment failed");
+            vec_f_commitments.push(f_commitment);
         }
 
-        prove::<Bn254>(&vec_f, &vec_omega, &domain, &mut transcript_prover);
+        let proof = prove::<Bn254>(&vec_f, &vec_omega, &domain, &mut transcript_prover, &ck);
+        let is_valid = verify::<Bn254>(proof, &vec_f_commitments, &vec_omega, &domain, &mut transcript_verifier, &vk);
+        assert!(is_valid);
     }
 }
 
