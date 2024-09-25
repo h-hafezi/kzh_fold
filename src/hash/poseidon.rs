0461db80d39d3229961b68ae8b1a61fec508394b
George Kadianakis <desnacked@riseup.net>
2024-09-25 12:25:56 +0300

    Start abstracting

--- src/hash/poseidon.rs
+++ src/hash/poseidon.rs
@@ -23,34 +23,38 @@ pub trait PoseidonHashTrait<F: Absorb + PrimeField> {
     fn output(&mut self) -> F;
 }
 
+pub fn get_poseidon_config<F: PrimeField>() -> PoseidonConfig<F> {
+    // 120 bit security target as in
+    // https://eprint.iacr.org/2019/458.pdf
+    // t = rate + 1
+
+    let full_rounds = 8;
+    let partial_rounds = 60;
+    let alpha = 5;
+    let rate = 4;
+
+    let (ark, mds) = find_poseidon_ark_and_mds::<F>(
+        F::MODULUS_BIT_SIZE as u64,
+        rate,
+        full_rounds,
+        partial_rounds,
+        0,
+        );
+    PoseidonConfig::new(
+        full_rounds as usize,
+        partial_rounds as usize,
+        alpha,
+        mds,
+        ark,
+        rate,
+        1,
+    )
+}
+
 impl<F: Absorb + PrimeField> PoseidonHashTrait<F> for PoseidonHash<F> {
     /// This Poseidon configuration generator agrees with Circom's Poseidon(4) in the case of BN254's scalar field
     fn new() -> Self {
-        // 120 bit security target as in
-        // https://eprint.iacr.org/2019/458.pdf
-        // t = rate + 1
-
-        let full_rounds = 8;
-        let partial_rounds = 60;
-        let alpha = 5;
-        let rate = 4;
-
-        let (ark, mds) = find_poseidon_ark_and_mds::<F>(
-            F::MODULUS_BIT_SIZE as u64,
-            rate,
-            full_rounds,
-            partial_rounds,
-            0,
-        );
-        let poseidon_params = PoseidonConfig::new(
-            full_rounds as usize,
-            partial_rounds as usize,
-            alpha,
-            mds,
-            ark,
-            rate,
-            1,
-        );
+        let poseidon_params = get_poseidon_config();
 
         Self {
             poseidon_params: poseidon_params.clone(),
