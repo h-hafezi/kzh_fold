49cd80e9f4f5a1a091863e11fcfc9d70fc37907d
h-hafezi <h.hafezi2000@gmail.com>
2024-12-24 21:26:58 +0330

    kzh3-fold bench

--- benches/bench_kzh2_fold.rs
+++ benches/bench_kzh2_fold.rs
@@ -6,11 +6,13 @@ use rand::thread_rng;
 
 use sqrtn_pcs::kzh_fold::kzh2_fold::{Acc2SRS, Accumulator2};
 use sqrtn_pcs::constant_for_curves::E;
+use sqrtn_pcs::kzh::KZH;
 use sqrtn_pcs::kzh::kzh2::{KZH2, KZH2SRS};
+use sqrtn_pcs::math::Math;
 use sqrtn_pcs::transcript::transcript::Transcript;
 
-fn get_srs(degree_x: usize, degree_y: usize) -> Acc2SRS<E> {
-    let srs_pcs: KZH2SRS<E> = KZH2::setup_1(degree_x, degree_y, &mut thread_rng());
+fn get_srs(degree: usize) -> Acc2SRS<E> {
+    let srs_pcs: KZH2SRS<E> = KZH2::setup(degree, &mut thread_rng());
 
     // return the result
     Accumulator2::setup(srs_pcs.clone(), &mut thread_rng())
@@ -19,24 +21,24 @@ fn get_srs(degree_x: usize, degree_y: usize) -> Acc2SRS<E> {
 
 fn bench_setup(c: &mut Criterion) {
     // Witness size: 2^2, 2^4 ..., 2^20
-    let degrees = vec![(2, 2), (4, 4), (8, 8), (16, 16), (32, 32), (64, 64), (128, 128), (256, 256), (512, 512), (1024, 1024)];
-    for (degree_x, degree_y) in degrees {
-        let bench_name = format!("setup for degrees n={} * m={} (witness size: {})", degree_x, degree_y, degree_x*degree_y);
+    let num_variables = vec![10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for degree in num_variables {
+        let bench_name = format!("setup for degrees n={}", degree);
         c.bench_function(&bench_name, |b| {
             b.iter(|| {
-                let _ = get_srs(degree_x, degree_y);
+                let _ = get_srs(degree);
             });
         });
     }
 }
 
 fn bench_prove(c: &mut Criterion) {
-    let degrees = vec![(2, 2), (4, 4), (8, 8), (16, 16), (32, 32), (64, 64), (128, 128), (256, 256), (512, 512), (1024, 1024)];
-    for (degree_x, degree_y) in degrees {
-        let srs = get_srs(degree_x, degree_y);
+    let num_variables = vec![10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for degree in num_variables {
+        let srs = get_srs(degree);
         let acc_1 = Accumulator2::rand(&srs, &mut thread_rng());
         let acc_2 = Accumulator2::rand(&srs, &mut thread_rng());
-        let bench_name = format!("prove for degrees n={} * m={} (witness size: {})", degree_x, degree_y, degree_x*degree_y);
+        let bench_name = format!("prove for degree n={}", degree);
         let mut transcript = Transcript::new(b"some label");
         c.bench_function(&bench_name, |b| {
             b.iter(|| {
@@ -47,15 +49,15 @@ fn bench_prove(c: &mut Criterion) {
 }
 
 fn bench_verify(c: &mut Criterion) {
-    let degrees = vec![(2, 2), (4, 4), (8, 8), (16, 16), (32, 32), (64, 64), (128, 128), (256, 256), (512, 512), (1024, 1024)];
-    for (degree_x, degree_y) in degrees {
-        let srs = get_srs(degree_x, degree_y);
+    let num_variables = vec![10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for degree in num_variables {
+        let srs = get_srs(degree);
         let acc_1 = Accumulator2::rand(&srs, &mut thread_rng());
         let acc_2 = Accumulator2::rand(&srs, &mut thread_rng());
         let mut prover_transcript = Transcript::new(b"some label");
         let mut verifier_transcript = prover_transcript.clone();
         let (_, _, Q) = Accumulator2::prove(&srs, &acc_1, &acc_2, &mut prover_transcript);
-        let bench_name = format!("verify for degrees n={} * m={} (witness size: {})", degree_x, degree_y, degree_x*degree_y);
+        let bench_name = format!("verify for degrees n={}", degree);
         c.bench_function(&bench_name, |b| {
             b.iter(|| {
                 let _ = Accumulator2::verify(&srs, &acc_1.instance, &acc_2.instance, Q, &mut verifier_transcript);
@@ -65,11 +67,11 @@ fn bench_verify(c: &mut Criterion) {
 }
 
 fn bench_decide(c: &mut Criterion) {
-    let degrees = vec![(2, 2), (4, 4), (8, 8), (16, 16), (32, 32), (64, 64), (128, 128), (256, 256), (512, 512), (1024, 1024)];
-    for (degree_x, degree_y) in degrees {
-        let srs = get_srs(degree_x, degree_y);
+    let num_variables = vec![10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    for degree in num_variables {
+        let srs = get_srs(degree);
         let acc = Accumulator2::rand(&srs, &mut thread_rng());
-        let bench_name = format!("decide for degrees n={} * m={} (witness size: {})", degree_x, degree_y, degree_x*degree_y);
+        let bench_name = format!("decide for degrees n={}", degree);
         c.bench_function(&bench_name, |b| {
             b.iter(|| {
                 let _ = Accumulator2::decide(&srs, &acc);
@@ -84,9 +86,9 @@ fn custom_criterion_config() -> Criterion {
 
 // Benchmark group setup
 criterion_group! {
-    name = acc_benches;
+    name = kzh2_fold_benches;
     config = custom_criterion_config();
     targets =  bench_verify, bench_prove, bench_decide, bench_setup
 }
 
-criterion_main!(acc_benches);
+criterion_main!(kzh2_fold_benches);
---
+++ benches/bench_kzh3_fold.rs
@@ -0,0 +1,76 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::thread_rng;
+use sqrtn_pcs::constant_for_curves::E;
+use sqrtn_pcs::kzh::KZH;
+use sqrtn_pcs::kzh::kzh3::{KZH3, KZH3SRS};
+use sqrtn_pcs::kzh_fold::kzh_3_fold::{Acc3SRS, Accumulator3};
+use sqrtn_pcs::transcript::transcript::Transcript;
+
+fn get_srs(degree: usize) -> Acc3SRS<E> {
+    let srs_pcs: KZH3SRS<E> = KZH3::setup(degree, &mut thread_rng());
+
+    // return the result
+    Accumulator3::setup(srs_pcs.clone(), &mut thread_rng())
+}
+
+fn bench_prove(c: &mut Criterion) {
+    let num_variables = vec![10];
+    for degree in num_variables {
+        let srs = get_srs(degree);
+        let acc_1 = Accumulator3::rand(&srs);
+        let acc_2 = Accumulator3::rand(&srs);
+        let bench_name = format!("prove for degree n={}", degree);
+        let mut transcript = Transcript::new(b"some label");
+        c.bench_function(&bench_name, |b| {
+            b.iter(|| {
+                let _ = Accumulator3::prove(&srs, &acc_1, &acc_2, &mut transcript);
+            })
+        });
+    }
+}
+
+
+fn bench_verify(c: &mut Criterion) {
+    let num_variables = vec![10];
+    for degree in num_variables {
+        let srs = get_srs(degree);
+        let acc_1 = Accumulator3::rand(&srs);
+        let acc_2 = Accumulator3::rand(&srs);
+        let mut prover_transcript = Transcript::new(b"some label");
+        let mut verifier_transcript = prover_transcript.clone();
+        let (_, _, proof) = Accumulator3::prove(&srs, &acc_1, &acc_2, &mut prover_transcript);
+        let bench_name = format!("verify for degrees n={}", degree);
+        c.bench_function(&bench_name, |b| {
+            b.iter(|| {
+                let _ = Accumulator3::verify(&srs, &acc_1.instance, &acc_2.instance, &proof, &mut verifier_transcript);
+            })
+        });
+    }
+}
+
+fn bench_decide(c: &mut Criterion) {
+    let num_variables = vec![10];
+    for degree in num_variables {
+        let srs = get_srs(degree);
+        let acc = Accumulator3::rand(&srs);
+        let bench_name = format!("decide for degrees n={}", degree);
+        c.bench_function(&bench_name, |b| {
+            b.iter(|| {
+                let _ = Accumulator3::decide(&srs, &acc);
+            })
+        });
+    }
+}
+
+fn custom_criterion_config() -> Criterion {
+    Criterion::default().sample_size(10)
+}
+
+// Benchmark group setup
+criterion_group! {
+    name = kzh3_fold_benches;
+    config = custom_criterion_config();
+    targets =  bench_verify, bench_prove, bench_decide
+}
+
+criterion_main!(kzh3_fold_benches);
